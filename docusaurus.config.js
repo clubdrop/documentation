974b105782f7c80af19b3fcdee6804b24baa47d1
clubdrop <clubdrop@outlook.com>
2022-12-02 15:18:41 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/clubdrop/documentation/tree/main/docs/',
+            'https://github.com/clubdrop/documentation/tree/main/',
             // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/clubdrop/documentation/tree/main/docs/',
+            'https://github.com/clubdrop/documentation/tree/main/',
             // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
